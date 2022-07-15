99e8a47478c300725b6c170873046c2c450d86f1
Anna <zharova1992@gmail.com>
2022-07-16 01:03:55 +0400

    задача 43

--- Program.cs
+++ Program.cs
@@ -42,5 +42,29 @@ ShowArray(array);
 int Pozitiv = PoziEl (array);
 Console.WriteLine("Количество чисел больше 0: " + Pozitiv); */
 
+/* Решение задачи 43
 
+void Point ();
+{
+    Console.WriteLine("Введите значение переменной b1: ");
+    int a = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите значение переменной k1: ");
+    int b = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите значение переменной b2: ");
+    int c = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите значение переменной k2: ");
+    int d = Convert.ToInt32(Console.ReadLine());  
+
+    int x, y;
+
+    if (b != d)
+    {
+        x = (c-a)/(b-d);
+        y = b * x + a;
+        Console.WriteLine("точка пересечения: (", +x ,";", +y,")");
+    }
+    else Console.WriteLine("поскольку k1 = k2 - прямые являются параллельными.");
+}
+
+Point (); */
 
